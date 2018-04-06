934a58500680b651aebd92be762f52a1cfcb0c6d
Tushaar Kamat <platypusdiamond@gmail.com>
2018-04-06 00:20:34 -0400

    Compiled for gh-pages

--- js/compiled/app.js
+++ js/compiled/app.js
@@ -7,6 +7,4 @@ document.write('<script>if (typeof goog == "undefined") console.warn("ClojureScr
 document.write('<script>goog.require("devtools.preload");</script>');
 document.write('<script>goog.require("re_frisk.preload");</script>');
 document.write('<script>goog.require("process.env");</script>');
-
-document.write("<script>if (typeof goog != \"undefined\") { goog.require(\"figwheel.connect.build_dev\"); }</script>");
 document.write('<script>goog.require("maze_evolution.core");</script>');
